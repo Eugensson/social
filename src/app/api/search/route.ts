3414ac94f3f4b81921fa38d419268938841b674d
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2024-09-25 17:35:25 +0300

    Added search feature

---
+++ src/app/api/search/route.ts
@@ -0,0 +1,64 @@
+import { NextRequest } from "next/server";
+
+import prisma from "@/lib/prisma";
+import { validateRequest } from "@/auth";
+import { getPostDataInclude, PostsPage } from "@/lib/types";
+
+export const GET = async (req: NextRequest) => {
+  try {
+    const q = req.nextUrl.searchParams.get("q") || "";
+    const cursor = req.nextUrl.searchParams.get("cursor") || undefined;
+
+    const searchQuery = q.split(" ").join(" & ");
+
+    const PAGE_SIZE = 10;
+
+    const { user } = await validateRequest();
+
+    if (!user) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const posts = await prisma.post.findMany({
+      where: {
+        OR: [
+          {
+            content: {
+              search: searchQuery,
+            },
+          },
+          {
+            user: {
+              displayName: {
+                search: searchQuery,
+              },
+            },
+          },
+          {
+            user: {
+              username: {
+                search: searchQuery,
+              },
+            },
+          },
+        ],
+      },
+      include: getPostDataInclude(user.id),
+      orderBy: { createdAt: "desc" },
+      take: PAGE_SIZE + 1,
+      cursor: cursor ? { id: cursor } : undefined,
+    });
+
+    const nextCursor = posts.length > PAGE_SIZE ? posts[PAGE_SIZE].id : null;
+
+    const data: PostsPage = {
+      posts: posts.slice(0, PAGE_SIZE),
+      nextCursor,
+    };
+
+    return Response.json(data);
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
